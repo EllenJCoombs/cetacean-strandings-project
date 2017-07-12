9accef0aadb8d168a429273ecc90b704cec562df
Natalie Cooper <ncooper@tcd.ie>
2017-07-12 18:22:42 +0100

    Simple code to make hex grid maps of strandings

---
+++ plots/grids-strandings.R
@@ -0,0 +1,38 @@
+# Plot strandings as hex grids rather than points
+# Because it looks cool :)
+
+# Extract UK map
+uk <- map_data("world", region = "UK")
+
+# Create base map
+gg1 <- 
+  ggplot() + 
+  geom_polygon(data = uk, aes(x = long, y = lat, group = group), 
+               fill = "white", color = "black") + 
+  coord_fixed(1.3) 
+
+# Read in the strandings data
+ds <- read_csv("cleaned-data/cleandatesnames.csv")
+
+# Remove NAs from coordinates
+# And restrict to things in UK waters
+ds <- ds %>%
+  filter(!is.na(Latitude) & !is.na(Longitude)) %>%
+  filter(Latitude < 65 & Latitude > 45) %>%
+  filter(Longitude < 3 & Latitude > -8)
+
+# Basic plot using viridis colour scheme
+# Note that you can change bins and transparency
+gg1+
+geom_hex(data = ds, aes(y = Latitude, x= Longitude), bins = 25, alpha = 0.5) +
+scale_fill_gradientn(colours = viridis(4))
+
+# More complex plot, with axes removed, smaller bins, defined colours, and simpler legend
+gg1+
+  geom_hex(data = ds, aes(y = Latitude, x= Longitude), bins = 50, alpha = 0.5) +
+  scale_fill_gradientn(colours = c("blue", "red")) +
+  theme(axis.line  = element_blank(),
+        axis.text  = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank()) +
+  guides(fill = guide_colorbar(title = NULL, ticks = FALSE))
