a4d694692a5b599f8fcfdbaefba4ca9d7ff658e2
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2017-07-26 17:09:41 +0100

    A few small changes - not the best dataset to use

--- EN4_Data.R
+++ EN4_Data.R
@@ -32,6 +32,7 @@ attributes(ncin2$var)$names
 print(ncin1)
 print(ncin2)
 
+ncin1
 
 ncin1_data <- ncvar_get(ncin1, attributes(ncin1$var)$names[1])
 print(ncin1_data)
@@ -103,7 +104,7 @@ image(long1,lat1,tmp_slice, col=rev(brewer.pal(10,"RdBu")))
 #All temps 
 # levelplot of the slice - doesn't look right 
 grid <- expand.grid(lon=long1, lat=lat1)
-cutpts <- c(-50,-40,-30,-20,-10,0,10,20,30,40,50)
+cutpts <- c(-20,-10,0,10,20,30)
 levelplot(tmp_slice ~ long1 * lat1, data=grid, at=cutpts, cuts=11, pretty=T, 
           col.regions=(rev(brewer.pal(10,"RdBu"))))
 
@@ -152,4 +153,88 @@ length(tmp_vec)
 tmp_df01 <- data.frame(cbind(lonlat1,tmp_vec))
 names(tmp_df01) <- c("lon1","lat1",paste(temp1,as.character(m), sep="_"))
 head(na.omit(tmp_df01), 10)
+
+
+
+
+install.packages("ncdf4.helpers")
+library(ncdf4.helpers)
+install.packages("PCICt")
+library(PCICt)
+
+#This use of non-standard calendars messes up the calculations of as.Date, 
+#so you must use a more specialized function to read in the time variable of the
+#climate model output and map it to standard calendar dates. 
+#You can do this with the nc.get.time.series function from the ncdf4.helpers package
+#(you should also load the PCICt package to use this function):
+
+tmp_time <- nc.get.time.series(ncin1, v = "sst",
+                               time.dim.name = "time")
+sst_time[c(1:3, length(sst_time) - 2:0)]
+
+#Getting sst data 
+sst <- ncvar_get(ncin, "sst")
+dim(sst)
+length(sst_time)
+summary(sst_time)
+
+#Selecting a specific time point and place 
+#Answers are in celcius? CHECK
+
+
+#Selecting a specific lat/long section 
+lon_index <- which(ncin$dim$lon$vals > -11 | ncin$dim$lon$vals < 3)
+lat_index <-  which(ncin$dim$lat$vals > 49 & ncin$dim$lat$vals < 60.9)
+
+
+#Selecting only one point 
+lon_index <- which.min(abs(lon - 0.3))
+lat_index <- which.min(abs(lat - 54.2))
+time_index <- which(format(sst_time, "%Y-%m-%d") == "2011-03-16")
+sst[lon_index, lat_index, time_index]
+
+
+#Picking out specific of data - this is one lat/long from 1870-2017
+#Note that this code uses format and as.Date to convert the PCICt object 
+#to a date object, to allow use of a date axis when plotting with ggplot2.
+lon_index <- which.min(abs(lon - 0.3))
+lat_index <- which.min(abs(lat - 54.2))
+sst <- nc.get.var.subset.by.axes(ncin, "sst",
+                                 axis.indices = list(X = lon_index,
+                                                     Y = lat_index))
+data_frame(time = sst_time, 
+           sst = as.vector(sst)) %>%
+  mutate(time = as.Date(format(time, "%Y-%m-%d"))) %>%
+  ggplot(aes(x = time, y = sst)) + 
+  geom_line() + 
+  xlab("Date") + ylab("Temperature (C)") + 
+  ggtitle("Daily measured sea-surface-temperature, 1870 - 2017",
+          subtitle = "At model grid point nearest Scarborough, United Kingdom") + 
+  theme_classic()
+
+
+#Modelling temperature for a single day - need to change the code as this currently 
+#gives a global picture 
+library(ggmap)
+library(viridis)
+install.packages("weathermetrics")
+library(weathermetrics)
+
+time_index <- which(format(sst_time, "%Y-%m-%d") == "2016-07-16")
+sst <- nc.get.var.subset.by.axes(ncin, "sst",
+                                 axis.indices = list(T = time_index))
+expand.grid(lon, lat) %>%
+  rename(lon = Var1, lat = Var2) %>%
+  mutate(lon = ifelse(lon > 180, -(360 - lon), lon),
+         sst = as.vector(sst)) %>% 
+  #mutate(sst = convert_temperature(sst, "k", "c")) %>%
+  ggplot() + 
+  geom_point(aes(x = lon, y = lat, color = sst),
+             size = 0.8) + 
+  borders("world", colour="black", fill=NA) + 
+  scale_color_viridis(name = "Temperature (C)") + 
+  theme_void() + 
+  coord_quickmap() + 
+  ggtitle("Measured temperature on 1977-07-16",
+          subtitle = "HadISST1") 
 
