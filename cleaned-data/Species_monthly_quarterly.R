156b5f987cca07cfb813973a913121a95e9e08c0
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2017-08-08 10:46:01 +0100

    Looking at different species strandings by month/quarter

--- cleaned-data/Species_monthly_quarterly.R
+++ cleaned-data/Species_monthly_quarterly.R
@@ -20,8 +20,8 @@ write.csv(cleaneddata, file = "cleandatesnames.csv")
 #Now Date should be 'Date' class 
 
 #Selecting the chosen area (if required)
-L_acutus <- cleaneddata %>% 
-  filter(Name.Current.Sci == "Lagenorhynchus acutus") 
+pphocoena <- cleaneddata %>% 
+  filter(Name.Current.Sci == "Phocoena phocoena") 
 
 #Use a variation of the following code if you want to select a specfic window 
 #nw_window <- nw_scotland_strandings %>% 
@@ -31,7 +31,7 @@ L_acutus <- cleaneddata %>%
 #ggplot(nw_window, aes(x = Date)) + 
   #stat_count(width = 0.5) 
 
-la_window <- L_acutus %>%
+pp_window <- pphocoena %>%
   filter(Year %in% c(1913:2015))
 
 
@@ -40,32 +40,26 @@ library(zoo)
 library(dplyr)
 
 #Splitting my data into strandings per month 
-la_window$monthYear <- as.Date(as.yearmon(la_window$Date))
-la_window$quarterYear <- as.Date(as.yearqtr(la_window$Date))
-head(la_window)
+pp_window$monthYear <- as.Date(as.yearmon(pp_window$Date))
+pp_window$quarterYear <- as.Date(as.yearqtr(pp_window$Date))
+head(pp_window)
 #Strandings gathered into months 
-la_monthly <- head(la_window %>% group_by(monthYear) %>% summarise(n = n()), 424)
+pp_monthly <- head(pp_window %>% group_by(monthYear) %>% summarise(n = n()), 7667)
 #Strandings per quarter 
-la_quarter <- head(la_window %>% group_by(quarterYear) %>% summarise(n = n()), 199)
+pp_quarter <- head(pp_window %>% group_by(quarterYear) %>% summarise(n = n()), 818)
 
 #Grouping by week number - not really needed - not running properly
-la_window$week <- as.Date("1913-03-06")+7*trunc((nw_window$joinTimestamp / 1000)/(3600*24*7))
+pp_window$week <- as.Date("1913-01-01")+7*trunc((pp_window$joinTimestamp / 1000)/(3600*24*7))
 
 #Line plot of strandings per month (grouped)
-ggplot(data = la_monthly, aes(x = monthYear, y = n, group=1)) +
+ggplot(data = pp_monthly, aes(x = monthYear, y = n, group=1)) +
   geom_line()
 
-ggplot() +
+ggplot(la_monthly, aes(n)) +
+  geom_histogram(binwidth = 0.5)
   
 
-#Looking at individual years 
-#Just use above code and change the years 
 
-ggplot(data = nw_monthly, aes(x = monthYear, y = n, group=1)) +
-  geom_line()
-
-#Need to use this if plot window isn't opening
-dev.off()
 
 
 
