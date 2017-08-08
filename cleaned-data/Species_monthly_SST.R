446a29ecad406731e6217fc8d623dbd6bcd641e9
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2017-08-08 16:35:44 +0100

    Attempting to plot montly strandings against monthly SST (HadISST1)

---
+++ cleaned-data/Species_monthly_SST.R
@@ -0,0 +1,152 @@
+#Splitting data into monthly/quarterly time slots 
+
+#Using the cleaneddata dataset 
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+
+
+cleaneddata <- read.csv("cleandatesnames.csv")
+sapply(cleaneddata, class)
+
+#Having problems with the data format - need dates to be 'date' class
+cleaneddata <- mutate(cleaneddata, Date = dmy(Date))
+#Trying this instead - works by changing all 2000s to 1900s
+#cleaneddata <- mutate(cleaneddata, Date = format(as.Date(Date, "%d-%b-%y"), "19%y-%m-%d"))
+
+write.csv(cleaneddata, file = "cleandatesnames.csv")
+
+#Now Date should be 'Date' class 
+
+#Selecting the chosen area (if required)
+pphocoena <- cleaneddata %>% 
+  filter(Name.Current.Sci == "Phocoena phocoena") 
+
+#Use a variation of the following code if you want to select a specfic window 
+#nw_window <- nw_scotland_strandings %>% 
+  #filter(Year %in% c(1989:2000))
+
+#Shows this datset by year but I need it by month.....
+#ggplot(nw_window, aes(x = Date)) + 
+  #stat_count(width = 0.5) 
+
+pp_window <- pphocoena %>%
+  filter(Year %in% c(1913:2015))
+
+
+install.packages("zoo")
+library(zoo)
+library(dplyr)
+
+#Splitting my data into strandings per month 
+pp_window$monthYear <- as.Date(as.yearmon(pp_window$Date))
+pp_window$quarterYear <- as.Date(as.yearqtr(pp_window$Date))
+head(pp_window)
+#Strandings gathered into months 
+pp_monthly <- head(pp_window %>% group_by(monthYear) %>% summarise(n = n()), 7667)
+#Strandings per quarter 
+pp_quarter <- head(pp_window %>% group_by(quarterYear) %>% summarise(n = n()), 818)
+
+#Grouping by week number - not really needed - not running properly
+pp_window$week <- as.Date("1913-01-01")+7*trunc((pp_window$joinTimestamp / 1000)/(3600*24*7))
+
+#Line plot of strandings per month (grouped)
+#Have changed monthYear to Date 
+ggplot(data = pp_monthly, aes(x = Date, y = n, group=1)) +
+  geom_line()
+
+ggplot(pp_monthly, aes(n)) +
+  geom_histogram(binwidth = 0.5)
+  
+
+##########################################################################################
+#Plotting species against temperature 
+#Example here is Phocoena monthly strandings against STT monthly temps (UK averaged 
+#from 14 locations)
+
+#Monthly stranding data is from the above code 
+#SST is from the HadISST_Uk.R code 
+
+UK_Ireland_SST <- read.csv("UK_Ireland_SST.csv")
+
+#Renaming time to Date 
+UK_Ireland_SST <- UK_Ireland_SST %>%
+  dplyr::rename(Date = time)
+
+#Selecting only the UK_mean 
+UK_mean_SST <- UK_Ireland_SST %>%
+  select(Date, UK_mean) 
+
+#Selecting only 1913:2015
+#Need to make Data a date first 
+UK_mean_SST <- mutate(UK_mean_SST, Date = ymd(Date))
+sapply(UK_mean_SST, class)
+
+write.csv(UK_mean_SST, file = "UK_mean_SST.csv")
+
+#Filteeing out specific dates 
+UK_mean_SST <- UK_mean_SST %>%
+  filter(Date >= "1912-12-16", Date <= "2016-01-16")
+
+
+#Plotting UK mean temperature 
+#Need to play with the months!
+bb1 <- ggplot() + 
+  geom_line(data = UK_mean_SST, aes(x = Date, y = UK_mean, group = 1)) + 
+  #geom_smooth(se = FALSE) +
+  #scale_colour_gradient(low='yellow', high='#de2d26') +
+  labs(y = "SST",
+       x = "time") +
+  ylim(0,17) +
+  ggtitle("Daily measured sea-surface-temperature, 1870 - 2017",   
+          subtitle = "UK mean") + 
+  theme_classic()
+
+bb1
+
+#Plotting strandings against temp 
+
+#Changing "monthYear" to "Date" in PP data 
+pp_monthly <- pp_monthly %>%
+  dplyr::rename(Date = monthYear)
+  
+  
+  #PPhocoena monthly strandings and monthly SST
+  ggplot() + 
+    geom_line(data = UK_mean_SST, aes(x = Date, y = UK_mean)) + 
+    geom_line(data = pp_monthly, aes(x = Date, y = n/4)) +
+    scale_y_continuous(sec.axis = sec_axis(~.*4, name = "Monthly strandings")) +
+    labs(y = "SST ("~degree~"C)",
+         x = "Year")
+
+#Any way of binding the two datasets? 
+#library(plyr)
+#combined <- rbind.fill(UK_mean_SST[c("Date", "UK_mean")], pp_monthly[c("Date", "n")])
+
+#Making the date for temperature the first of the month (for that whole month) 
+#UK_mean_01_month <- UK_mean_SST
+
+#Make the SST yearly 
+#UK_mean_01_month$monthYear <- as.Date(as.yearmon(UK_mean_01_month$Date))
+#UK_mean_01_month$quarterYear <- as.Date(as.yearqtr(UK_mean_01_month$Date))
+#head(UK_mean_01_month)
+
+#Line plot of strandings per month (grouped)
+#Have changed monthYear to Date 
+#ggplot(data = UK_mean_01_month, aes(x = monthYear, y = UK_mean, group=1)) +
+  #geom_line()
+
+
+#library(plyr)
+#combined <- rbind.fill(UK_mean_01_month[c("monthYear", "UK_mean")], pp_monthly[c("Date", "n")])
+combined <- UK_mean_01_month %>% 
+  select(UK_mean, monthYear) %>%
+  dplyr::rename(Date = monthYear)
+  
+  
+combined <- merge(combined, pp_monthly, all = TRUE, by = c('Date'))
+
+ggplot(data = combined, aes(x = UK_mean, y = n)) +
+  geom_point(size = 0.5) +
+  labs(x = "Sea Surface Temperature (~degrees~ (C))", y = "Monthly Phocoena phocoena strandings")
+
