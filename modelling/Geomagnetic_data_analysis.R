e440ff217d039c2f8171ec4e9ce448b1dedd4317
Ellen Coombs <ellen.coombs.14@ucl.ac.uk>
2017-09-05 11:14:06 +0100

    Attempt at bringing in multiple .txt files

--- modelling/Geomagnetic_data_analysis.R
+++ modelling/Geomagnetic_data_analysis.R
@@ -7,6 +7,7 @@
 library(dplyr)
 library(tidyr)
 
+#This is the global geomag data
 geomag <- read.csv("Geomagnetic_data.csv")
 labels(geomag)
 
@@ -38,3 +39,43 @@ geomag_yearly_max <- geomag_yearly_max %>%
 ggplot(data = geomag_yearly_max, aes(x = Year, y = x, group = 1)) +
   geom_line() +
   labs(y = "Kp value")
+
+
+##################################################################################################
+#Reading in multiple data files from UK oberservatories 
+#For Lerwick (LER) and Eskdalemuir (ESK) this is 1940 – present
+#For Hartland (HAD), this folder contains data for Greenwich (GRW 1868 - 1925) 
+#and Abinger (ABN 1926 - 1956) both based in and near London before these observatory 
+#operations were moved to Hartland (1957 – present) in Devon when the electrification of 
+#tramlines and railway disturbances made measurements in London impossible
+
+#Each data file contains a year of K-index data. This is a 3-houly scale of local geomagnetic activity, weighted to its locality. 
+#The scale ranged from 0 (quiet) – 9 (most disturbed)
+#The file format is:
+#Day        Month  Year       Day-number      Eight 3-hourly K-index values
+
+
+require(readr)  # for read_csv()
+require(dplyr)  # for mutate()
+require(tidyr)  # for unnest()
+require(purrr)  # for map(), reduce()
+
+#First, the Eskdalemuir (ESK) files 
+files_esk <- dir(pattern = "*.esk")
+files_esk
+
+data_esk <- files_esk %>% 
+ map_dfr(files_esk)
+
+test <- dir(pattern = "*.txt")
+test
+data_test <- test %>%  
+  map_dfr(data_test)
+
+data_test <- test %>%
+  map(read_delim, delim = "\t") %>%    
+  reduce(rbind)     
+data_esk
+
+
+
