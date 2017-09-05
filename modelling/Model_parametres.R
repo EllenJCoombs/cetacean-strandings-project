50f8d820d8bfdd20568f7e3caf09bf1de51f75ef
Ellen Coombs <ellen.coombs.14@ucl.ac.uk>
2017-09-05 15:35:37 +0100

    Added geomag (global), population and storm counts

--- modelling/Model_parametres.R
+++ modelling/Model_parametres.R
@@ -20,3 +20,38 @@ storms <- storms %>%
 #Population data 
 #This is a data file that is combined with yearly strandings 
 
+Population <- read.csv("Population_UK.csv")
+
+
+###################################################################################################
+#Global max magnetic data 
+
+geomag <- read.csv("Geomagnetic_data.csv")
+
+sapply(geomag, class)
+#Changing Date to dat (ymd)
+geomag <- mutate(geomag, Date = ymd(Date))
+
+#Take a yearly average?? 
+#Firstly split the data in to year, month and day 
+geomag <- separate(geomag, "Date", c("Year", "Month", "Day"), sep = "-") 
+#Take average by year 
+geomag_yearly_mean <- aggregate(k ~ Year, FUN=mean, data=geomag)
+#monthly mean (I'm not sure yearly shows enough definition)
+geomag_monthly_mean <- aggregate(k ~ Year + Month, FUN=mean, data=geomag) %>%
+  arrange(Year)
+
+#Yearly max Kp values 
+geomag_yearly_max <- aggregate(geomag$k, by = list(geomag$Year), max)
+#Renaming the columns
+geomag_yearly_max <- geomag_yearly_max %>%
+  dplyr::rename(Year = Group.1)
+
+write.csv(geomag_yearly_max, file = "Geomag_yearly_global_max.csv")
+
+
+##################################################################################################
+#Species richness 
+
+cleaneddata <- read.csv("cleandatesnames.csv")
+
