edbac0083ddffc7b33a2115782c2a3733984f01f
Ellen Coombs <ellen.coombs.14@ucl.ac.uk>
2017-09-15 11:38:54 +0100

    Having a look at organisational data and interpreting the GAM outputs

--- modelling/GAMs.R
+++ modelling/GAMs.R
@@ -5,11 +5,11 @@ Model_data <- read.csv("Model_data.csv")
 Model_data$X <- NULL 
 
 
-b_t <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k=4), data=Model_data, method = "REML",
+b_a <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k=4), data=Model_data, method = "REML",
            family=poisson())
 
-summary(b_t)
-plot(b_t)
+summary(b_a)
+plot(b_a)
 
 
 # not enough unique values of storms? How many are there? 
@@ -24,70 +24,74 @@ unique(Model_data$Organisations)
 #when we only have 4 different categories)
 
 #Here this model is looking at Richness, offset by population and the smooths are "Year" and "Storms"
-b_t <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k=4), data=Model_data, method = "REML",
+b_b <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k=4), data=Model_data, method = "REML",
            family=poisson())
+
+summary(b_b)
 
 #Just looking at geogmagnetic data and year 
 unique(Model_data$Max_K_index)
+plot(b_b)
 
+#Geomagnetic data 
 #It has 5 categories so using a K value of 4 
-b_t <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Max_K_index, k=4), data=Model_data, method = "REML",
+b_c <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Max_K_index, k=4), data=Model_data, method = "REML",
            family=poisson())
 
-summary(b_t)
-plot(b_t)
+summary(b_c)
+plot(b_c)
 
 #Almost identical to storm data 
 
 #Modelling all variables - magnets, storms, year 
-b_t <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k = 4) +s(Max_K_index, k=4), data=Model_data, method = "REML",
+b_d <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k = 4) +s(Max_K_index, k=4), data=Model_data, method = "REML",
            family=poisson())
 
-summary(b_t)
-plot(b_t)
+summary(b_d)
+plot(b_d)
 
 # get the AIC - the lower the better - this gives you an idea of whether extra parametres added 
 # to a model are worth it for the added analysis/complexity, or whether the simpler model with fewer 
 # parametres is better 
 
-AIC(b_t)
+AIC(b_d)
 
 par(mfrow=c(2,2))
 # compare k' column to edf if close, double k
-gam.check(b_t)
+gam.check(b_d)
 
 
 # residuals look wacky (quantile-quantile plot not matching red line)
 # increasing variance in resids. vs. linear pred. plot
 # try quasi poisson?
-b_t <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k = 4) +s(Max_K_index, k=4), data=Model_data, method = "REML",
+b_e <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k = 4) +s(Max_K_index, k=4), data=Model_data, method = "REML",
            family=quasipoisson())
 
-summary(b_t)
-plot(b_t)
+summary(b_e)
+plot(b_e)
 
 par(mfrow=c(2,2))
-gam.check(b_t)
+gam.check(b_e)
 
 
 # lets be fancy!
 # Tweedie
 # set a=1.2 because things get weird if less than 1.2
-b_t <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k = 4) +s(Max_K_index, k=4), data=Model_data, method = "REML",
+b_f <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k = 4) +s(Max_K_index, k=4), data=Model_data, method = "REML",
            family=tw(a=1.2))
 
-summary(b_t)
-plot(b_t)
+summary(b_f)
+plot(b_f)
 
 par(mfrow=c(2,2))
-gam.check(b_t)
+gam.check(b_f)
 
 # make predictions - useful if I have loads of missing data 
-b_t <- gam(Richness ~ offset(log(Population)) + s(Year), data=Model_data, method = "REML",
+b_g <- gam(Richness ~ offset(log(Population)) + s(Year), data=Model_data, method = "REML",
            family=tw(a=1.2))
 
 plot_data <- Model_data
-predict_Richness <- predict(b_t, newdata=Model_data, type="response", se.fit=TRUE)
+predict_Richness <- predict(b_g, newdata=Model_data, type="response", se.fit=TRUE)
 plot_data$predict_Richness <- predict_Richness$fit
 plot_data$upper_Richness <- predict_Richness$fit + 2*predict_Richness$se.fit
 plot_data$lower_Richness <- predict_Richness$fit - 2*predict_Richness$se.fit
@@ -103,7 +107,7 @@ rug(plot_data$Year)
 #Playing around with data that has missing values (here richness during and around the war years)
 test_data <- read.csv("test_data.csv")
 
-b_t <- gam(Richness ~ offset(log(Population)) + s(Year), data=Model_data, method = "REML",
+b_h <- gam(Richness ~ offset(log(Population)) + s(Year), data=Model_data, method = "REML",
            family=tw(a=1.2))
 
 plot_data <- test_data
@@ -126,27 +130,29 @@ rug(plot_data$Year)
 #Here this model is looking at Richness, offset by population and the smooths 
 #are "Year" and "Organisations"
 
-b_t <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Organisations), data=Model_data, method = "REML",
+b_i <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Organisations), data=Model_data, method = "REML",
            family=tw(a=1.2))
 
-summary(b_t)
-plot(b_t)
+summary(b_i)
+plot(b_i)
 
 par(mfrow=c(2,2))
-gam.check(b_t)
+gam.check(b_i)
 
-AIC(b_t)
+AIC(b_i)
 
 #Shoving everything into the model 
-b_t <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k = 4) +s(Max_K_index, k=4) +s(Organisations), data=Model_data, method = "REML",
+b_j <- gam(Richness ~ offset(log(Population)) + s(Year) +s(Storms, k = 4) +s(Max_K_index, k=4) +s(Organisations), data=Model_data, method = "REML",
            family=tw(a=1.2))
 
-summary(b_t)
-plot(b_t)
+summary(b_j)
+plot(b_j)
 
 par(mfrow=c(2,2))
-gam.check(b_t)
+gam.check(b_j)
 
 #This gives an AIC score 3 points higher - is the model complexity really worth it? 
 #Model only explains .3 more of the variation 
-AIC(b_t)
+AIC(b_i, b_j)
+anova(b_i)
+anova(b_j)
