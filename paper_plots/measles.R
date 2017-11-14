1280cee62aaf7916e2ddb7211a3c62dc871681ba
David Lawrence Miller <github@ninepointeightone.net>
2017-11-14 16:09:37 +0000

    remove axis label, make text italic

--- paper_plots/measles.R
+++ paper_plots/measles.R
@@ -25,13 +25,14 @@ p <- ggplot(plotdat) +
   geom_tile(aes(x=Year, y=Species, fill=total)) +
   scale_x_continuous(expand=c(0,0)) +
   theme_minimal() +
-  labs(fill="Individuals") +
+  labs(fill="Individuals", y="") +
   # needs to be on the log scale because of pho^2 :(
   scale_fill_viridis(trans = "log", na.value="white",
                      limits = c(1, 501),
                      breaks = c(1, 10, 20, 50, 100, 250, 500),
                      labels = c(1, 10, 20, 50, 100, 250, 500)) +
-  theme(legend.position="bottom", legend.key.width=unit(0.1, "npc"))
+  theme(legend.position="bottom", legend.key.width=unit(0.1, "npc"),
+        axis.text.y=element_text(face="italic"))
 
 print(p)
 
