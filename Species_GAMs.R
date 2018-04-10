6618e2a2d796afd4d7d9d1463b088c093723653c
Ellen Coombs <ellen.coombs.14@ucl.ac.uk>
2018-04-10 15:14:09 +0100

    Splitting data into species

--- Species_GAMs.R
+++ Species_GAMs.R
@@ -3,3 +3,20 @@
 #Species GAMs 
 #This code runs the GAMs for each of the indiviual species 
 
+#First, code to pick out each of the seperate species 
+
+
+
+#Now we need to take each species, give it a count per year (including 0s for years it wasn't 
+#recorded) and add it to the model data 
+
+#Make new dataframe by splitting into seperate species (these are tibbles)
+lDf <- split(sep_species, sep_species$Species)
+
+#Example of calling up sei whale data 
+lDf$`Balaenoptera borealis`
+
+#Running code for each of the seperate species 
+#There are 21 species, it might be easiest to just plug each of the species dataframes (above) into the 
+#following code 
+
