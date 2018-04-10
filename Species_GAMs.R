5a49c8df023057e27ca4b866aadc9e67b0689f41
Ellen Coombs <ellen.coombs.14@ucl.ac.uk>
2018-04-10 17:16:59 +0100

    Running species plots

--- Species_GAMs.R
+++ Species_GAMs.R
@@ -44,26 +44,27 @@ ZC <- Y[[21]] #Cuvier's
 
 
 #Now the GAM's for each species 
+#Use the above acronyms e.g., 'TT' 
 #Doing this one at a time - but I'm sure there is a way o do this and then use broom....
 
-BA_GAM <- gam(Total_strandings ~ offset(log(Population)) +
+GG_GAM <- gam(Total_strandings ~ offset(log(Population)) +
                         s(Year, bs="ts") +
                         s(Storms, k=5, bs="ts") +
                         s(Max_K_index, k=4, bs="ts") +
                         s(Max_SST, bs="ts") +
                         s(NAO_index, bs="ts"), 
-                      data= BA, 
+                      data= GG, 
                       method= "REML",
                       family=nb)
 
 
-summary(BA_GAM)
+summary(GG_GAM)
 par(mfrow = c(2,2))
-plot(BA_GAM)
+plot(GG_GAM)
 
 #Gam.check
 par(mfrow=c(2,2))
-gam.check(BA_GAM)
+gam.check(GG_GAM)
 
 
 
