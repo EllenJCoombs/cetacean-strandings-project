3e22d34d085c68e8d851c323666cfed4533681c4
Ellen Coombs <ellen.coombs.14@ucl.ac.uk>
2018-06-15 15:04:14 +0100

    Adding text to ggplot2

--- paper_plots/REVISED_megaplot_with_key.R
+++ paper_plots/REVISED_megaplot_with_key.R
@@ -1,9 +1,20 @@
 
+install.packages("gridExtra")
+
 library(dplyr)
 library(ggplot2)
+library(gridExtra)
+library(grid)
+library(gtable)
 
 #This code plots new Figure 1 (line plot)
 #New code on 15/06/2018 with the key underneath 
+
+#Create text grob 
+Text1 = textGrob(paste("Blah Blah Blah, 
+                       yadda yadda yadda,
+                       booooo"))
+
 # get the data
 linesdat <- ds %>%
   group_by(Year, whatareyou) %>%
@@ -22,6 +33,7 @@ plines <- ggplot(linesdat) +
   #position = "jitter" + 
   theme_minimal() +
   theme(legend.position= "right") +
+  theme(axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0))) +
   labs(x="", y="Total stranded individuals", colour="Suborder")
 
 #Adding events to the plot 
@@ -33,43 +45,21 @@ plines <- plines + annotate("rect", xmin=1914, xmax=1918, ymin=0, ymax=800, alph
   annotate("rect", xmin=1939, xmax=1945, ymin=0, ymax=800, alpha=.1, fill="gray22") +
   geom_text(
     aes(x = 1942, y = 780, label = "WWII"), size = 3, colour = "gray38") + 
-  annotate("segment", x =1985, xend=1985, y=130, yend=750, colour = "gray38", size=0.5, arrow=arrow(length=unit(0.1,"cm"))) +
   geom_text(
-    aes(x = 1988, y = 770, label = "1985/1986 season:
-        Moratorium on whaling comes into effect"), size = 3, colour = "gray38") +
-  #Annotation coordinates 
-  annotate("segment", x = 1990, xend = 1990, y = 230, yend = 585, colour="gray38", size=0.5, arrow=arrow(length=unit(0.1,"cm"))) + 
+    aes(x = 1990, y = 230, label = "5"), size = 3, colour = "gray38") +
   geom_text(
-    aes(x = 1992, y = 610, label = "1990: CSIP and IWDG
-        programmes start"), size = 3, colour = "gray38") +
-  annotate("segment", x = 1947, xend = 1947, y = 40, yend = 550, colour="gray38", size=0.5, arrow=arrow(length=unit(0.1,"cm"))) + 
+    aes(x = 1992, y = 360, label = "6"), size = 3, colour = "gray38") +
   geom_text(
-    aes(x = 1953, y = 430, label = "1950s: Increase in 
-        post-war fishing & 
-        whaling effort"), size = 3, colour = "gray38") + 
-  annotate("segment", x = 1953, xend = 1953, y = 40, yend = 400, colour="gray38", size=0.5, arrow=arrow(length=unit(0.1,"cm"))) + 
+    aes(x = 1953, y = 50, label = "3"), size = 3, colour = "gray38") +
   geom_text(
-    aes(x = 1946, y = 590, label = "1946: NATO military testing 
-        in European waters:
-        submarine, military sonar,
-        and torpedo testing increase"), size = 3, colour = "gray38") +
-  annotate("segment", x = 1920, xend = 1920, y = 40, yend = 400, colour="gray38", size=0.5, arrow=arrow(length=unit(0.1,"cm"))) + 
+    aes(x = 1946, y = 50, label = "2"), size = 3, colour = "gray38") +
   geom_text(
-    aes(x = 1920, y = 420, label = "1920s: Sonar is used 
-        in UK and French waters"), size = 3, colour = "gray38") +
-  annotate("segment", x = 2000, xend = 2000, y = 40, yend = 110, colour="gray38", size=0.5, arrow=arrow(length=unit(0.1,"cm"))) + 
+    aes(x = 1920, y = 50, label = "1"), size = 3, colour = "gray38") + 
   geom_text(
-    aes(x = 2000, y = 140, label = "2000s: Increase in 
-        pile-driving for offshore 
-        wind turbines"), size = 3, colour = "gray38") +
-  annotate("segment", x = 1960, xend = 1960, y = 40, yend = 90, colour="gray38", size=0.5, arrow=arrow(length=unit(0.1,"cm"))) + 
+    aes(x = 2000, y = 500, label = "7"), size = 3, colour = "gray38") + 
   geom_text(
-    aes(x = 1960, y = 140, label = "1960s: Increase in
-        use of polychlorinated 
-        biphenyls (PCBs)
-        and other chemical 
-        pollutants"), size = 3, colour = "gray38")
-
+    aes(x = 1960, y = 50, label = "4"), size = 3, colour = "gray38")
+  
 
 #Adding circles for mass stranding years 
 plines <- plines + geom_point(aes(x=1950, y=275), size=10, shape=1, color="black") + 
@@ -84,5 +74,14 @@ plines <- plines + geom_point(aes(x=1950, y=275), size=10, shape=1, color="black
   geom_point(aes(x=2015, y=667), size=10, shape=1, color="gray29")
 
 
-#Run the plot
+# Add a row below the 2nd from the bottom
+gp = ggplotGrob(plines)
+gp = gtable_add_rows(gp, unit(3, "grobheight", Text1), -3)
+# Add 'lab' grob to that row, under the plot panel
+gp = gtable_add_grob(gp, Text1, t = -3, l = gp$layout[gp$layout$name == "panel",]$l)
+
+grid.newpage()
+grid.draw(gp)
+
+#Run the plot - this runs it without the text underneath 
 plines
