ca8a466a71292705bfb1a16a4d8d76781bf51e41
Ellen Coombs <ellen.coombs.14@ucl.ac.uk>
2018-11-05 14:21:29 +0000

    1990s data

--- cleaned-data/1990s_data.R
+++ cleaned-data/1990s_data.R
@@ -1,2 +1,10 @@
 
 #1990s data only using the data with the rare species left in as confident with species IDs
+
+#Chop data to just 1990 onwards 
+UK_and_Irish_sp <- read.csv("UK_and_Irish_sp.csv") #without rares (we've justifed why species)
+
+#Filter data to just CSIP and IWDG (post 1990s)
+
+
+
